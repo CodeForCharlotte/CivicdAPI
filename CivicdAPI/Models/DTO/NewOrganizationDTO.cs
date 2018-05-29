e42509418a57343b4cea6b0274370310af0eb105
Eleanor Palmer <ePalmer@hhogdev.com>
2018-05-29 17:48:55 -0400

    Changed OrganizationCategory for DTO to int. Added user to role depending on OrganizationCategory

--- CivicdAPI/Models/DTO/NewOrganizationDTO.cs
+++ CivicdAPI/Models/DTO/NewOrganizationDTO.cs
@@ -6,24 +6,24 @@ using System.Web;
 
 namespace CivicdAPI.Models.DTO
 {
-    public class NewOrganizationDTO
-    {
-        [Required]
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string DisplayName { get; set; }
-        [Required]
-        public string Password { get; set; }
-        [Required]
-        public OrganizationCategory OrganizationCategory { get; set; }
-        public string PhoneNumber { get; set; }
-        public string ProfileDescription { get; set; }
-        public string StreetAddressOne { get; set; }
-        public string StreetAddressTwo { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string ZipCode { get; set; }
-        public IEnumerable<TagDTO> Tags { get; set; }
-    }
+  public class NewOrganizationDTO
+  {
+    [Required]
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string DisplayName { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public int OrganizationCategory { get; set; }
+    public string PhoneNumber { get; set; }
+    public string ProfileDescription { get; set; }
+    public string StreetAddressOne { get; set; }
+    public string StreetAddressTwo { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public IEnumerable<TagDTO> Tags { get; set; }
+  }
 }
