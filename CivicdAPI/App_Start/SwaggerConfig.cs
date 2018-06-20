38054b23c37c26201f6a54a45ae96551bbeb0bf5
dpatrickGithub <dustin.patrick@levvel.io>
2018-06-19 20:38:08 -0400

    Update xml doc location and configure release build.

--- CivicdAPI/App_Start/SwaggerConfig.cs
+++ CivicdAPI/App_Start/SwaggerConfig.cs
@@ -100,7 +100,7 @@ namespace CivicdAPI
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        c.IncludeXmlComments(() => new System.Xml.XPath.XPathDocument(AppDomain.CurrentDomain.BaseDirectory + @"\App_Data\XmlDocument.xml"));
+                        c.IncludeXmlComments(() => new System.Xml.XPath.XPathDocument(AppDomain.CurrentDomain.BaseDirectory + @"\bin\CivicdAPI.xml"));
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
